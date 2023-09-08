d3808720e75fce39c7c8c3a9248fd647d8ce09ef
tokologo1 <tokocha81@gmail.com>
2023-09-08 15:08:15 +0200

    update

--- 0X0A-primegame/0-prime_game.py
+++ 0X0A-primegame/0-prime_game.py
@@ -1,56 +1,34 @@
 #!/usr/bin/python3
-
-""" Prime Game Algorithm Python """
-
-
-def is_prime(n):
-    """ Checks if a number given n is a prime number """
-    for i in range(2, int(n ** 0.5) + 1):
-        if not n % i:
-            return False
-    return True
-
-
-def calculate_primes(n, primes):
-    """ Calculate all primes """
-    top_prime = primes[-1]
-    if n > top_prime:
-        for i in range(top_prime + 1, n + 1):
-            if is_prime(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+"""Given consecutive integers starting from 1 up to and including n"""
 
 
 def isWinner(x, nums):
-    """
-    x is the number of rounds and nums is an array of n
-    Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    """
+    """Given consecutive integers starting from 1 up to and including n"""
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
 
-    players_wins = {"Maria": 0, "Ben": 0}
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
 
-    primes = [0, 0, 2]
-
-    calculate_primes(max(nums), primes)
-
-    for round in range(x):
-        sum_options = sum((i != 0 and i <= nums[round])
-                          for i in primes[:nums[round] + 1])
-
-        if (sum_options % 2):
-            winner = "Maria"
-        else:
-            winner = "Ben"
-
-        if winner:
-            players_wins[winner] += 1
-
-    if players_wins["Maria"] > players_wins["Ben"]:
-        return "Maria"
-    elif players_wins["Ben"] > players_wins["Maria"]:
-        return "Ben"
-
-    return None
+    winner = ''
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        winner = None
+    if player1 * 2 > len(nums):
+        winner = "Maria"
+    else:
+        winner = "Ben"
+    return winner
